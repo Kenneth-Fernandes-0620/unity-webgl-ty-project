4f4479926705bd282d33f28a593475c996d0dfaa
KENNETH <kennethdominic57@gmail.com>
2021-09-21 10:24:43 +0530

    fixed not rendering error
    Signed-off-by: KENNETH <kennethdominic57@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 module.exports = app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
